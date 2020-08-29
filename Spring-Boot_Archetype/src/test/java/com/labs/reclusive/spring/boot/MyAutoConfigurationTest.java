4337bf66c3fd210eb13bd0709251999f6f6329a6
Alexander Hopgood <alex.hopgood@gmail.com>
2020-08-29 21:11:14 +0100

    Migrating Spring-Boot_Archetype to JUnit 5 Jupiter. As a consequence updating the spring version to 5.0.4.RELEASE and spring boot to 2.0.0.RELEASE.

--- Spring-Boot_Archetype/src/test/java/com/labs/reclusive/spring/boot/MyAutoConfigurationTest.java
+++ Spring-Boot_Archetype/src/test/java/com/labs/reclusive/spring/boot/MyAutoConfigurationTest.java
@@ -1,18 +1,18 @@
 package com.labs.reclusive.spring.boot;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(SpringRunner.class)
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @EnableAutoConfiguration
 public class MyAutoConfigurationTest {
@@ -20,16 +20,16 @@ public class MyAutoConfigurationTest {
     @Autowired
     MyProperties props;
 
-    @Before
+    @BeforeEach
     public void setUp() throws Exception {
     }
 
-    @After
+    @AfterEach
     public void tearDown() throws Exception {
     }
 
     @Test
-    public void myProperties() throws Exception {
+    void myProperties() throws Exception {
         assertEquals("alex", props.getName());
         assertEquals((Long)2L, props.getVersion());
     }

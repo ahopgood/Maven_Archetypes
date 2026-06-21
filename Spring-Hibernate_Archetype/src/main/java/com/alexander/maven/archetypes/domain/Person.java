f19a33ac9b37fd70d04f0e849e9b7c83e571163c
Alex Hopgood <alex.hopgood@gmail.com>
2026-06-21 16:35:28 +0100

    Moving to spring-boot-dependencies common BOM, compile successful

--- Spring-Hibernate_Archetype/src/main/java/com/alexander/maven/archetypes/domain/Person.java
+++ Spring-Hibernate_Archetype/src/main/java/com/alexander/maven/archetypes/domain/Person.java
@@ -9,8 +9,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.hibernate.annotations.NaturalId;
 
 /**
